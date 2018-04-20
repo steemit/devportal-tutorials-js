3ec2433476b4181ec2cf9281f598d7b879eb5382
Feruz M <fmiboy@gmail.com>
2018-04-20 21:50:32 +0300

    04 Tutorial Get posts (#47)
    * resolves #16, get posts with different category

---
+++ tutorials/04_get_posts/public/app.js
@@ -0,0 +1,41 @@
+const dsteem = require('dsteem')
+
+let opts = {}
+
+//connect to production server
+opts.addressPrefix = 'STM'
+opts.chainId = '0000000000000000000000000000000000000000000000000000000000000000'
+
+//connect to server which is connected to the network/production
+const client = new dsteem.Client('https://api.steemit.com')
+
+//filter change selection function
+window.filtersChange = async() => {
+    
+    filter = document.getElementById("filters").value
+    const query = {
+        tag: '',
+        limit: 5
+    };
+    client.database.getDiscussions(filter, query).then((result) => {
+        if (result){
+            var posts = [];
+            result.forEach( (post) => {
+                const json = JSON.parse(post.json_metadata);
+                const image = json.image?json.image[0]:'';
+                const title = post.title;
+                const author = post.author;
+                const created = new Date(post.created).toDateString();
+                posts.push(
+                    `<div class="list-group-item"><h4 class="list-group-item-heading">${title}</h4><p>by ${author}</p><center><img src="${image}" class="img-responsive center-block" style="max-width: 450px"/></center><p class="list-group-item-text text-right text-nowrap">${created}</p></div>`
+                )
+            });
+
+            document.getElementById('postList').innerHTML = posts.join('');
+        }
+        
+    }).catch((err) => {
+        console.log(err);
+        alert('Error occured, try again');
+    });
+}
---
+++ tutorials/04_get_posts/webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: ['./public/app.js', './public/style.scss'],
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js',
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    performance: {
+        hints: process.env.NODE_ENV === 'production' ? 'warning' : false,
+    },
+};
