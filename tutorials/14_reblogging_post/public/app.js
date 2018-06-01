a994004fea41bdf1e3f64c3f2e8943fe6ec6f63d
Donovan Walker <donovan@donovanwalker.com>
2018-06-01 17:21:32 -0500

    [#25] improved UI & code explanation

--- tutorials/14_reblogging_post/public/app.js
+++ tutorials/14_reblogging_post/public/app.js
@@ -5,72 +5,98 @@ let opts = {};
 opts.addressPrefix = 'STM';
 opts.chainId =
     '0000000000000000000000000000000000000000000000000000000000000000';
-//connect to server which is connected to the network/testnet
+//connect to a Steem node. This is currently setup on production, but we recommend using a testnet like https://testnet.steem.vc
 const client = new dsteem.Client('https://api.steemit.com', opts);
 
-// This function is taken from the tutorial https://github.com/steemit/devportal-tutorials-js/blob/master/tutorials/01_blog_feed/
+//This is a convenience function for the UI.
+window.autofillAuthorAndPermlink = function(el) {
+    console.log('thank you for using autofill', el);
+    document.getElementById('theAuthor').value = el.dataset.author;
+    document.getElementById('thePermLink').value = el.dataset.permlink;
+};
 
 function fetchBlog() {
     const query = {
         tag: 'steemitblog',
         limit: 5,
     };
+
     client.database
-        .getDiscussions('blog', query)
+        .getDiscussions('blog', query) //get a list of posts for easy resteeming.
         .then(result => {
-            var posts = [];
+            //when the response comes back ...
+            const postList = [];
+            console.log('Listing blog posts by ' + query.tag);
             result.forEach(post => {
+                //... loop through the posts ...
                 const author = post.author;
                 const permlink = post.permlink;
-                posts.push(
-                    `<li><a href="javascript://" onclick="document.getElementById('theAuthor').value = '${author}'">${author}</a> - <a href="javascript://" onclick="document.getElementById('thePermLink').value = '${permlink}'">${permlink}</a></li>`
+                console.log(author, permlink, post);
+                postList.push(
+                    // and render the table rows
+                    `<tr><td><button class="btn-sm btn-success" data-author="${author}" data-permlink="${permlink}" onclick="autofillAuthorAndPermlink(this)">Autofill</button></td><td>${author}</td><td>${permlink}</td></tr>`
                 );
             });
 
-            document.getElementById('postList').innerHTML = posts.join('');
+            document.getElementById('postList').innerHTML = postList.join('');
         })
         .catch(err => {
+            console.error(err);
             alert('Error occured' + err);
         });
 }
 
-window.onload = fetchBlog();
-
 //this function will execute when the HTML form is submitted
 window.submitPost = async () => {
+    resteemOutput('preparing to submit');
     //get private key
-    const privateKey = dsteem.PrivateKey.fromString(
-        document.getElementById('postingKey').value
-    );
-    //get account name
-    const myAccount = document.getElementById('username').value;
-    //get blog author
-    const theAuthor = document.getElementById('theAuthor').value;
-    //get blog permLink
-    const thePermLink = document.getElementById('thePermLink').value;
+    try {
+        console.log(document.getElementById('postingKey').value);
+        const privateKey = dsteem.PrivateKey.fromString(
+            document.getElementById('postingKey').value
+        );
+        console.log('success');
+        //get account name
+        const myAccount = document.getElementById('username').value;
+        //get blog author
+        const theAuthor = document.getElementById('theAuthor').value;
+        //get blog permLink
+        const thePermLink = document.getElementById('thePermLink').value;
 
-    const jsonOp = JSON.stringify([
-        'reblog',
-        {
-            account: myAccount,
-            author: theAuthor,
-            permlink: thePermLink,
-        },
-    ]);
+        const jsonOp = JSON.stringify([
+            'reblog',
+            {
+                account: myAccount,
+                author: theAuthor,
+                permlink: thePermLink,
+            },
+        ]);
 
-    const data = {
-        id: 'follow',
-        json: jsonOp,
-        required_auths: [],
-        required_posting_auths: [myAccount],
-    };
-
-    client.broadcast.json(data, privateKey).then(
-        function(result) {
-            console.log('client broadcast result: ', result);
-        },
-        function(error) {
-            console.error(error);
-        }
-    );
+        const data = {
+            id: 'follow',
+            json: jsonOp,
+            required_auths: [],
+            required_posting_auths: [myAccount],
+        };
+        resteemOutput('resteeming:\n', JSON.stringify(data, 2));
+        console.log('resteeming:', data);
+        client.broadcast.json(data, privateKey).then(
+            function(result) {
+                resteemOutput(result);
+                console.log('resteem result: ', result);
+            },
+            function(error) {
+                console.error(error);
+            }
+        );
+    } catch (e) {
+        resteemOutput(e.message);
+        console.log(e);
+    }
 };
+
+function resteemOutput(output) {
+    document.getElementById('results').innerText = output;
+}
+
+window.onload = fetchBlog;
