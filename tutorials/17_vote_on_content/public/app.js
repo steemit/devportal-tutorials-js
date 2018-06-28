427066490191e89cb6a0e3489c4354c089ca9c23
Robert Oswald <robo@shiftbit.io>
2018-06-28 21:36:29 +0200

    Rework UI and some logic

--- tutorials/17_vote_on_content/public/app.js
+++ tutorials/17_vote_on_content/public/app.js
@@ -2,10 +2,10 @@ const dsteem = require('dsteem');
 //define network parameters
 let opts = {};
 opts.addressPrefix = 'STX';
-opts.chainId = '79276aea5d4877d9a25892eaa01b0adf019d3e5cb12a97478df3298ccdd01673';
+opts.chainId =
+    '79276aea5d4877d9a25892eaa01b0adf019d3e5cb12a97478df3298ccdd01673';
 //connect to a steem node, testnet in this case
 const client = new dsteem.Client('https://testnet.steem.vc', opts);
-
 
 // const dsteem = require('dsteem');
 // let opts = {};
@@ -15,7 +15,6 @@ const client = new dsteem.Client('https://testnet.steem.vc', opts);
 //     '0000000000000000000000000000000000000000000000000000000000000000';
 // //connect to a steem node, production in this case
 // const client = new dsteem.Client('https://api.steemit.com');
-
 
 //refer to "10_submit_post" in the tutorials folder for creating a post on steemit
 //create post function
@@ -27,11 +26,11 @@ window.createPost = async () => {
     //get account name
     const account = document.getElementById('username').value;
     //get title
-    const title = "New Blog";
+    const title = 'New Blog';
     //get body
-    const body = "This is my new test blog";
+    const body = 'This is my new test blog';
     //get tags and convert to array list
-    const tags = "blog";
+    const tags = 'blog';
     const taglist = tags.split(' ');
     //make simple json metadata including only tags
     const json_metadata = JSON.stringify({ tags: taglist });
@@ -56,14 +55,15 @@ window.createPost = async () => {
         .then(
             function(result) {
                 document.getElementById('permlink').innerHTML = permlink;
-                document.getElementById('postLink').style.display = 'block';
                 document.getElementById(
                     'postLink'
-                ).innerHTML = `<br/><p>Included in block: ${
-                    result.block_num
-                }</p><br/><br/><a href="http://condenser.steem.vc/${
+                ).innerHTML = `Included in block: <a href="http://condenser.steem.vc/${
                     taglist[0]
-                }/@${account}/${permlink}">Check post here</a>`;
+                }/@${account}/${permlink}" target="_blank">${
+                    result.block_num
+                }</a>`;
+                document.getElementById('postResult').style.display = 'flex';
+                document.getElementById('permlink').value = permlink;
             },
             function(error) {
                 console.error(error);
@@ -71,12 +71,9 @@ window.createPost = async () => {
         );
 };
 
-
-
-
 //submit vote function executes when you click "Submit Vote" button
 window.submitVote = async () => {
-    document.getElementById('voteResult').innerHTML = 'pending...'
+    document.getElementById('voteResult').innerHTML = 'pending...';
     //get all values from the UI
     //get account name of voter
     const voter = document.getElementById('username').value;
@@ -87,25 +84,51 @@ window.submitVote = async () => {
     //get author of post/comment to vote
     const author = document.getElementById('author').value;
     //get post permalink to vote
-    const permlink = document.getElementById('permlink').innerHTML;
+    const permlink = document.getElementById('permlink').value;
     //get weight of vote
-    const weight = parseInt(document.getElementById('currentWeight').innerHTML, 10);
-    
+    const weight = parseInt(
+        document.getElementById('currentWeight').innerHTML,
+        10
+    );
+
     //create vote object
     const vote = new Object();
-      vote.voter = voter;
-      vote.author = author;
-      vote.permlink = permlink;
-      vote.weight = weight; //needs to be an integer for the vote function
+    vote.voter = voter;
+    vote.author = author;
+    vote.permlink = permlink;
+    vote.weight = weight; //needs to be an integer for the vote function
 
     //broadcast the vote
 
-    client.broadcast.vote(vote, privateKey).then(function(result){
-        console.log('included in block: ' + result.block_num, 'expired: ' + result.expired)
-        document.getElementById('voteResult').innerHTML = 'Success'
-    }, function(error) {
-        console.error(error)
-        document.getElementById('voteResult').innerHTML = 'Failed'
-    })
+    client.broadcast.vote(vote, privateKey).then(
+        function(result) {
+            console.log(
+                'included in block: ' + result.block_num,
+                'expired: ' + result.expired
+            );
+            document.getElementById('voteResultContainer').style.display =
+                'flex';
+            document.getElementById('voteResult').className =
+                'form-control-plaintext alert alert-success';
+            document.getElementById('voteResult').innerHTML = 'Success';
+        },
+        function(error) {
+            console.error(error);
+            document.getElementById('voteResultContainer').style.display =
+                'flex';
+            document.getElementById('voteResult').className =
+                'form-control-plaintext alert alert-danger';
+            document.getElementById('voteResult').innerHTML =
+                error.jse_shortmsg;
+        }
+    );
+};
 
+window.onload = () => {
+    var voteWeightSlider = document.getElementById('voteWeight');
+    var currentWeightDiv = document.getElementById('currentWeight');
+    currentWeightDiv.innerHTML = voteWeightSlider.value;
+    voteWeightSlider.oninput = function() {
+        currentWeightDiv.innerHTML = this.value;
+    };
 };
