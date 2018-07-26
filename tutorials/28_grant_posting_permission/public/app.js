a19e7e2666a9b539446e7c8289d1b8d514472f0c
shiftb1t <git@shiftbit.io>
2018-07-26 13:55:14 +0200

    rework UI and logic. Update readme

--- tutorials/28_grant_posting_permission/public/app.js
+++ tutorials/28_grant_posting_permission/public/app.js
@@ -16,19 +16,14 @@ const client = new dsteem.Client('https://testnet.steem.vc', opts);
 // //connect to a steem node, production in this case
 // const client = new dsteem.Client('https://api.steemit.com');
 
-
-//grant posting permission function
-window.submitPermission = async () => {
-    //input from UI
+//check permission status
+window.submitCheck = async () => {
     //get username
     const username = document.getElementById('username').value;
-    //get private active key
-    const privateKey = dsteem.PrivateKey.fromString(
-        document.getElementById('privateKey').value
-    );
     //get account to provide posting auth
     const newAccount = document.getElementById('newAccount').value;
-   
+
+    //query database for posting array
     _data = new Array
     _data = await client.database.getAccounts([username]);
     const postingAuth = _data[0].posting;
@@ -39,80 +34,120 @@ window.submitPermission = async () => {
     for (var i = 0,len = checkAuth.length; i<len; i++) {
         if (checkAuth[i][0]==newAccount) {
             arrayindex = i
+            var checktext = "User already has posting permission"
+        } else {
+            var checktext = "User does not yet have posting permission"
         }
+    }
+    document.getElementById('permCheckContainer').style.display = 'flex';
+    document.getElementById('permCheck').className = 'form-control-plaintext alert alert-success';
+    document.getElementById('permCheck').innerHTML = checktext;
+}
+
+//grant permission function
+window.submitPermission = async () => {
+    //get username
+    const username = document.getElementById('username').value;
+    //get private active key
+    const privateKey = dsteem.PrivateKey.fromString(
+        document.getElementById('privateKey').value
+    );
+    //get account to provide posting auth
+    const newAccount = document.getElementById('newAccount').value;
+
+    _data = new Array
+    _data = await client.database.getAccounts([username]);
+    const postingAuth = _data[0].posting;
+
+    //adding of new account to posting array
+    postingAuth.account_auths.push([newAccount, parseInt(postingAuth.weight_threshold)]);
+    
+    //object creation
+    const accObj = {
+        account: username,
+        json_metadata: _data[0].json_metadata,
+        memo_key: _data[0].memo_key,
+        posting: postingAuth,
     }
     
-    if (arrayindex > -1) {
-        
-        document.getElementById('permCheckContainer').style.display = 'flex';
-        document.getElementById('permCheck').className = 'form-control-plaintext alert alert-success';
-        document.getElementById('permCheck').innerHTML = "User already has posting permission, do you want to revoke?";
-        document.getElementById("submitCancelBtn").style.visibility = "visible";
-        document.getElementById("submitRevokeBtn").style.visibility = "visible";
-
-        window.submitRevoke = async () => {
-            //removal of array element in order to revoke posting permission
-            postingAuth.account_auths = postingAuth.account_auths.splice(0,1);
-            //object creation
-            const accObj = {
-                account: username,
-                json_metadata: _data[0].json_metadata,
-                memo_key: _data[0].memo_key,
-                posting: postingAuth,
-            }
-            //account update broadcast
-            client.broadcast.updateAccount(accObj, privateKey).then(
-                function(result) {
-                    console.log(
-                        'included in block: ' + result.block_num,
-                        'expired: ' + result.expired
-                    );
-                    document.getElementById('permCheckContainer').style.display = 'flex';
-                    document.getElementById('permCheck').className = 'form-control-plaintext alert alert-success';
-                    document.getElementById('permCheck').innerHTML = 'Permission removed';
-                },
-                function(error) {
-                    console.error(error);
-                    document.getElementById('permCheckContainer').style.display = 'flex';
-                    document.getElementById('permCheck').className = 'form-control-plaintext alert alert-danger';
-                    document.getElementById('permCheck').innerHTML = error.jse_shortmsg;
-                }
+    //account update broadcast
+    client.broadcast.updateAccount(accObj, privateKey).then(
+        function(result) {
+            console.log(
+                'included in block: ' + result.block_num,
+                'expired: ' + result.expired
             );
-        };
-
-        window.submitCancel = async () => {
-
             document.getElementById('permCheckContainer').style.display = 'flex';
             document.getElementById('permCheck').className = 'form-control-plaintext alert alert-success';
-            document.getElementById('permCheck').innerHTML = "Operation cancelled";
-        };
-    } else {
-        //adding array element with new user name
-        postingAuth.account_auths.push([newAccount, parseInt(postingAuth.weight_threshold)]);
-        //object creation
-        const accObj = {
-            account: username,
-            json_metadata: _data[0].json_metadata,
-            memo_key: _data[0].memo_key,
-            posting: postingAuth,
+            document.getElementById('permCheck').innerHTML = "permission has been granted";
+        },
+        function(error) {
+            console.error(error);
+            document.getElementById('permCheckContainer').style.display = 'flex';
+            document.getElementById('permCheck').className = 'form-control-plaintext alert alert-danger';
+            document.getElementById('permCheck').innerHTML = error.jse_shortmsg;
         }
-        //account update broadcast
-        client.broadcast.updateAccount(accObj, privateKey).then(
-            function(result) {
-                console.log(
-                    'included in block: ' + result.block_num,
-                    'expired: ' + result.expired
-                );
-                document.getElementById('permCheckContainer').style.display = 'flex';
-                document.getElementById('permCheck').className = 'form-control-plaintext alert alert-success';
-                document.getElementById('permCheck').innerHTML = 'Permission granted';
-            },
-            function(error) {
-                console.error(error);
-                document.getElementById('permCheckContainer').style.display = 'flex';
-                document.getElementById('permCheck').className = 'form-control-plaintext alert alert-danger';
-                document.getElementById('permCheck').innerHTML = error.jse_shortmsg;
-            }
-        );
+    );
+}
+
+//revoke permission function
+window.submitRevoke = async () => {
+    //get username
+    const username = document.getElementById('username').value;
+    //get private active key
+    const privateKey = dsteem.PrivateKey.fromString(
+        document.getElementById('privateKey').value
+    );
+    //get account to provide posting auth
+    const newAccount = document.getElementById('newAccount').value;
+
+    _data = new Array
+    _data = await client.database.getAccounts([username]);
+    const postingAuth = _data[0].posting;
+
+    //check for user index in posting array
+    const checkAuth = _data[0].posting.account_auths;
+    var arrayindex = -1;
+    for (var i = 0,len = checkAuth.length; i<len; i++) {
+        if (checkAuth[i][0]==newAccount) {
+            arrayindex = i
+        }
+    }    
+
+    if (arrayindex = -1) {
+        document.getElementById('permCheckContainer').style.display = 'flex';
+        document.getElementById('permCheck').className = 'form-control-plaintext alert alert-danger';
+        document.getElementById('permCheck').innerHTML = "ERROR! User does not yet have permission and revoking the access will remove the next available user";
+        return;
     }
+
+    //removal of array element in order to revoke posting permission
+    postingAuth.account_auths.splice(arrayindex,1);
+
+    //object creation
+    const accObj = {
+        account: username,
+        json_metadata: _data[0].json_metadata,
+        memo_key: _data[0].memo_key,
+        posting: postingAuth,
+    }
+
+    //account update broadcast
+    client.broadcast.updateAccount(accObj, privateKey).then(
+        function(result) {
+            console.log(
+                'included in block: ' + result.block_num,
+                'expired: ' + result.expired
+            );
+            document.getElementById('permCheckContainer').style.display = 'flex';
+            document.getElementById('permCheck').className = 'form-control-plaintext alert alert-success';
+            document.getElementById('permCheck').innerHTML = "permission has been revoked";
+        },
+        function(error) {
+            console.error(error);
+            document.getElementById('permCheckContainer').style.display = 'flex';
+            document.getElementById('permCheck').className = 'form-control-plaintext alert alert-danger';
+            document.getElementById('permCheck').innerHTML = error.jse_shortmsg;
+        }
+    );
 };
